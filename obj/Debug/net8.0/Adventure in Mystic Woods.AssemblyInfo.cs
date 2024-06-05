78318a012d08bd1044d26d8f46751ec2b8052f9c
TorbjornF-H01 <torbjornfh01.kodehode@gmail.com>
2024-06-05 09:52:42 +0200

    moved pc, added gitignore

--- obj/Debug/net8.0/Adventure in Mystic Woods.AssemblyInfo.cs
+++ obj/Debug/net8.0/Adventure in Mystic Woods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adventure in Mystic Woods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b1a4916a0dbfe81aff5d051e8e6ebb46298711")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ad86e5daf8a3df48df798ec397f9d113e83c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adventure in Mystic Woods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adventure in Mystic Woods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
